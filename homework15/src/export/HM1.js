b132e7fbc9fd68fcf62a09c041a39120f173f1bf
unknown <madrigalamar@gmail.com>
2023-02-22 21:21:35 +0200

    add homeworks 12, 13

--- homework15/src/export/HM1.js
+++
@@ -1,9 +0,0 @@
-
-function randomDiscount (){
- const randomDiscount = Math.random();
-
-document.writeln(`<p> Випадкова знижка (використання
-функцї Math.random). : ${randomDiscount}</p>`); 
-
-  return randomDiscount;
-};
